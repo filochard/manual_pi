e21333684f82618e914c108c3d6ab26024e4be36
Alec Leamas <leamas.alec@gmail.com>
2024-10-11 09:52:21 +0200

    Flatpak: Add new 24.08 beta builds - part 2 (#598)

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -88,7 +88,10 @@ flatpak install --user -y --or-update --noninteractive \
     ${FLATHUB_REPO:-flathub}  org.opencpn.OpenCPN
 
 # Configure and build the plugin tarball and metadata.
-cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release} ..
+cmake \
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release} \
+    -DOPN_TARGET_TUPLE="flatpak-$(uname -m);${SDK};$(uname -m)" \
+    ..
 # Do not build flatpak in parallel; make becomes unreliable
 make -j 1 VERBOSE=1 flatpak
 
